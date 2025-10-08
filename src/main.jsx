552d86097bb234c9073fb49f1c6797b475d9b872
Vitor Fengler <vitor.fengler7@gmail.com>
2025-10-08 10:29:28 -0300

    alterando o auth0

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,10 @@ import { Auth0Provider } from '@auth0/auth0-react'
 
 createRoot(document.getElementById('root')).render(
   <Auth0Provider
-      domain="dev-vlbhbshl7b1pxe8e.us.auth0.com"
-      clientId="eo8WNkaTwrcGujuCMSkRZG3yMYuQvXTy"
+      domain="dev-t43vsk8g0llttnvt.us.auth0.com"
+      clientId="FNeGe3ZcaaH1IxJHBY6I9CqJMQqamE9o"
       authorizationParams={{
-        audience: "https://dev-vlbhbshl7b1pxe8e.us.auth0.com/api/v2/",
+        audience: "https://dev-t43vsk8g0llttnvt.us.auth0.com/api/v2/",
         redirect_uri: window.location.origin
       }}
     >
