2c27e3c5a7841707b629a27bc7729d4baeaac34e
Vitor Fengler <vitor.fengler7@gmail.com>
2025-10-08 10:46:44 -0300

    mudando base url

--- src/Stocks.jsx
+++ src/Stocks.jsx
@@ -3,7 +3,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
 
-const BASE_URL = "http://15.228.236.37:8080";
+const BASE_URL = "/api";
 
 
 export default function StocksApp() {
